2f1eedeacb59fa1a8b88e6bb7b417c4a0683adae
Tyler Dryden <dryden.inc718@gmail.com>
2025-04-22 18:20:09 -0400

    add exercise definition

--- Workout/models.py
+++ Workout/models.py
@@ -1,4 +1,50 @@
+import logging
 from django.db import models
+from datetime import date
+from accounts.models import Athlete
 
 # Create your models here.
 
+class ExerciseDefinition(models.Model):
+    name = models.CharField(blank=False, null=False, max_length=100)
+    video = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.id})"
+
+
+class ExercisePrescription(models.Model):
+    athlete_program_assignment = models.ForeignKey(
+        "AthleteProgramAssignment", on_delete=models.deletion.DO_NOTHING, null=True
+    )
+ 
+    exercise_definition = models.ForeignKey(
+        ExerciseDefinition, on_delete=models.CASCADE
+    )
+    sets = models.TextField(null=True, blank=True)
+    reps = models.TextField(null=True, blank=True)
+    load = models.TextField(null=True, blank=True)
+    duration = models.TextField(null=True, blank=True)
+    note = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.routine}: {self.exercise_definition}"
+
+
+class ExerciseLog(models.Model):
+    date = models.DateField(default=date.today)
+    exercise_prescription = models.ForeignKey(
+        ExercisePrescription, on_delete=models.CASCADE, null=True
+    )
+    athlete = models.ForeignKey(Athlete, models.CASCADE, default=None)
+
+    completed = models.BooleanField(null=True, blank=True)
+    note = models.TextField(null=True, blank=True)
+    sets = models.TextField(null=True, blank=True)
+    reps = models.TextField(null=True, blank=True)
+    load = models.TextField(null=True, blank=True)
+    duration = models.TextField(null=True, blank=True)
+
+
+    def __str__(self):
+        return f"{self.exercise_prescription}"
--- Workout/views.py
+++ Workout/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(["POST"])
+@csrf_exempt
+def create_exercise_definition(request):
+    data = request.data
+
+    name = data["name"] or None
+    type_of_exercise = data["type_of_exercise"] or None
+    video = data["video"] or None
+
+    if ExerciseDefinition.objects.filter(name=name).exists():
+        return Response(status=status.HTTP_412_PRECONDITION_FAILED,
+                        data={"reason": "An exercise definition with this name already exists."})
+
+    try:
+        ExerciseDefinition.objects.create(
+            name=name, type_of_exercise=type_of_exercise, video=video
+        ).save()
+    except Exception as e:
+        return Response(status=status.HTTP_400_BAD_REQUEST,
+                        data={"reason": f"Could not create exercise defintion: {str(e)}"})
+
+    return Response(status=status.HTTP_201_CREATED)
